11028f40d2d9870a0509cf1b74f955ff35a5c84e
mahaben <maha.benabbou93@gmail.com>
2015-12-02 17:03:56 +0100

    [Maha] Modify Covoiturage Controller

--- app/src/main/java/com/hardis/connect/controller/CovoiturageController.java
+++ app/src/main/java/com/hardis/connect/controller/CovoiturageController.java
@@ -35,98 +35,6 @@ public class CovoiturageController {
 
     private final static List<Covoiturage> covoiturages = new ArrayList<>();
 
-    public static void createCovoiturage(Covoiturage covoiturage, final Context context,final VolleyCallBack callBack) {
-        StringRequest request = null;
-        try {
-           final JSONObject jsonBody = new JSONObject("{\"Publication\":{\"Group\":{\"Id\":1},\"Category\":{\"Id\":1},\"Title\":\"Covoiturage vers Maroc\",\"Description\":\"Trajet en voiture à plusieurs\"},\"Capacity\":8,\"DepartureAgency\":{\"Id\":2},\"ArrivalAgency\":{\"Id\":1},\"DepartureTime\":\"2015-12-06T12:30:00\",\"ArrivalTime\":\"2015-12-07T15:30:00\"}");
-
-
-            request = new StringRequest(Request.Method.POST, AllUrls.add_covoiturage_url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.v("covoiturage", "yees");
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.v("covoiturage", "no");
-                    if (error instanceof TimeoutError) {
-                        Log.e("Volley", "TimeoutError");
-                    }else if(error instanceof NoConnectionError){
-                        Log.e("Volley", "NoConnectionError");
-                    } else if (error instanceof AuthFailureError) {
-                        Log.e("Volley", "AuthFailureError");
-                    } else if (error instanceof ServerError) {
-                        Log.e("Volley", "ServerError");
-                    } else if (error instanceof NetworkError) {
-                        Log.e("Volley", "NetworkError");
-                    } else if (error instanceof ParseError) {
-                        Log.e("Volley", "ParseError");
-                    }
-                }
-            }) {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<String, String>();
-                    SharedPreferences pref = context.getSharedPreferences("Hardis", 0);
-                    String token = pref.getString("token", null);
-                    Log.v("token retrieved", token);
-                    params.put("Authorization", token);
-                    return params;
-                }
-
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    Log.v("jsonbody",jsonBody.toString());
-                    return jsonBody.toString().getBytes();
-                }
-            };
-        }
-        catch(JSONException e)
-        {
-
-        }
-        /*try {
-            JSONObject jsonBody = new JSONObject("{\"Publication\":{\"Group\":{\"Id\":1},\"Category\":{\"Id\":1},\"Title\":\"Covoiturage vers Maroc\",\"Description\":\"Trajet en voiture à plusieurs\"},\"Capacity\":8,\"DepartureAgency\":{\"Id\":2},\"ArrivalAgency\":{\"Id\":1},\"DepartureTime\":\"2015-12-06T12:30:00\",\"ArrivalTime\":\"2015-12-07T15:30:00\"}");
-
-            request = new JsonObjectRequest(Request.Method.POST, AllUrls.add_covoiturage_url, jsonBody,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Log.v("covoiturage","yees");
-                            Log.v("covoi",response.toString());
-                            callBack.onSuccess("success");
-
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            VolleyLog.d("Response", error.getMessage());
-                            callBack.onFailed("failed");
-                        }
-                    }
-            )
-            {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<String, String>();
-                    SharedPreferences pref = context.getSharedPreferences("Hardis", 0);
-                    String token =pref.getString("token",null);
-                    Log.v("token retrieved", token);
-                    params.put("Authorization", token);
-                    return params;
-                }
-            };
-        }
-        catch(JSONException e)
-        {
-
-        }*/
-        RequestController.getInstance(context).addToRequestQueue(request);
-
-    }
-
     public static List<Covoiturage> getOffresCovoiturage(final Context context,final VolleyCallBack callBack) {
 
         StringRequest request = new StringRequest(Request.Method.GET, AllUrls.get_offres_covoiturage_url,
@@ -177,7 +85,7 @@ public class CovoiturageController {
         return covoiturages;
     }
 
-    public static void testAddTravel(Context context, final VolleyCallBack callBack) {
+    public static void testAddTravel(Covoiturage covoiturage,Context context, final VolleyCallBack callBack) {
         JsonObjectRequest request = null;
         try {
             JSONObject jsonObj = new JSONObject();
@@ -207,6 +115,7 @@ public class CovoiturageController {
                         @Override
                         public void onResponse(JSONObject response) {
                             callBack.onSuccess("success");
+                            Log.v("hoang","success");
 
                         }
                     },
@@ -228,7 +137,6 @@ public class CovoiturageController {
         }
         catch(JSONException e)
         {
-
         }
         RequestController.getInstance(context).addToRequestQueue(request);
     }
